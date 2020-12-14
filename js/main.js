02844cb88571fb2e1cd8f521c7e07d46d0f08548
Adrian <aolguin8097@gmail.com>
2020-12-13 23:00:36 -0800

    Added smooth scrolling, updating project section

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
     })
 
-    $('.project-area .button-group #btn1').trigger('click');
+    //$('.project-area .button-group #btn1').trigger('click');
 
     $('.project-area .grid .test-popup-link').magnificPopup({
         type: 'image',
